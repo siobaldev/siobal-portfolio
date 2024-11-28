47cbe4fa2104ccfbcba0d761d71d6c9106fc767b
br0wnrecluse <sblmnrd@gmail.com>
2024-11-28 17:19:19 +0800

    Initial setup

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "Minard Siobal | Portfolio",
+  description: "A portfolio to showcase my projects and skills/technologies.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
