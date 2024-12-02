62b6d83ade6a4ad448c11732039dd0145c96c7f6
br0wnrecluse <sblmnrd@gmail.com>
2024-12-03 03:29:48 +0800

    Added padding for responsiveness of homepage

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Header from "@/components/header/page";
 import Main from "@/components/main/page";
 export default function HomePage() {
   return (
-    <div className="container gap-y-20 flex flex-col lg:flex-row min-h-screen max-w-screen-[1200px] lg:px-24  text-white/87">
+    <div className="container lg:gap-y-20 flex flex-col lg:flex-row min-h-screen max-w-screen-[1200px] max-[400px]:px-6 gap-y-12 md:gap-y-0 sm:px-20 px-10 min-w-[320px] text-white/87">
       <Header />
       <Main />
     </div>
