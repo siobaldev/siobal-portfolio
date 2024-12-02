4149a5800442f4a99d9c2ad8fa42a585b8f319e4
br0wnrecluse <sblmnrd@gmail.com>
2024-12-03 03:34:23 +0800

    Added contact and fixed padding

--- components/main/page.jsx
+++ components/main/page.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import About from "./about/page";
 import Projects from "./projects/page";
 import Technologies from "./technologies/page";
+import Contact from "./contact/page";
 
 export default function Main() {
   return (
-    <main className="lg:w-1/2 space-y-36 py-14 md:py-20 lg:py-32">
+    <main className="lg:w-1/2 lg:space-y-36 space-y-20 md:space-y-28 mb-40 md:py-20 lg:py-32">
       <About />
       <Projects />
       <Technologies />
+      <Contact />
     </main>
   );
 }
