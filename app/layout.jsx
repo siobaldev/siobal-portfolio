bff8980f351e30c03ad59d729745b03afd621db3
br0wnrecluse <sblmnrd@gmail.com>
2024-12-03 19:38:41 +0800

    Added favicon for light and dark theme

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,6 +15,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/assets/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/assets/favicon/favicon.svg"
+        />
+        <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/favicon/apple-touch-icon.png"
+        />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Minard Siobal | Portfolio"
+        />
+        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} antialiased font-sans bg-primary`}
       >
