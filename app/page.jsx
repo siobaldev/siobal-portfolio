dd7376472e76ea1099e1420c4c32b56cd7ea281d
br0wnrecluse <sblmnrd@gmail.com>
2024-12-03 22:53:38 +0800

    Added noise gif bg
    Fixed light and dark favicon

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import Header from "@/components/header/page";
 import Main from "@/components/main/page";
+
 export default function HomePage() {
   return (
-    <div className="container lg:gap-y-20 flex flex-col lg:flex-row min-h-screen max-w-screen-[1200px] max-[400px]:px-6 gap-y-12 md:gap-y-0 sm:px-20 px-10 min-w-[320px] text-white/87">
+    <div className="container relative lg:gap-y-20 flex flex-col lg:flex-row min-h-screen max-w-screen-[1200px] max-[400px]:px-6 gap-y-12 md:gap-y-0 sm:px-20 px-10 min-w-[320px] text-white/87">
+      <div className="grain absolute flex items-center justify-center opacity-[0.01] h-full w-full bg-no-repeat bg-cover" />
       <Header />
       <Main />
     </div>
