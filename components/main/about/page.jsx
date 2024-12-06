65fb0ce27baca53b829a4c2cef820efa4cc70a77
br0wnrecluse <sblmnrd@gmail.com>
2024-12-06 09:23:52 +0800

    Added cat animation

--- components/main/about/page.jsx
+++ components/main/about/page.jsx
@@ -1,17 +1,57 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import HangingCat from "@/public/assets/hangingCat.svg";
 
 export default function About() {
+  const [isNekoVisible, setIsNekoVisible] = useState(true);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/assets/oneko.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  useEffect(() => {
+    const neko = document.getElementById("oneko");
+    if (neko) {
+      neko.style.display = isNekoVisible ? "block" : "none";
+    }
+  }, [isNekoVisible]);
+
+  const toggleNeko = () => {
+    setIsNekoVisible(!isNekoVisible);
+  };
+
   return (
     <section id="about" className="space-y-8 scroll-mt-32">
       <h1 className="uppercase text-sm font-bold w-fit tracking-widest hover:text-accent hover:opacity-100">
         about
       </h1>
-      <p className="opacity-60">
-        Hey! I&apos;m Minard, a front-end developer who started taking interest
-        in awesome websites in 2023. I specialize in UI design and crafting
-        engaging experiences with great attention to detail. <br /> <br />
-        When I&apos;m not coding, you&apos;ll find me reading comics, playing
-        AQW, or browsing meowrot(I love cats) contents.
+      <p>
+        <span className="opacity-60">
+          {" "}
+          Hey! I&apos;m Minard, a front-end developer who started taking
+          interest in awesome websites in 2023. I specialize in UI design and
+          crafting engaging experiences with great attention to detail. <br />{" "}
+          <br />
+          When I&apos;m not coding, you&apos;ll find me reading comics, playing
+          AQW, or browsing meowrot contents. I love{" "}
+        </span>
+        <span className="underline relative">
+          <span className="opacity-60">cats</span>
+          <HangingCat
+            className={`size-12 absolute -right-5  top-[16px] cursor-pointer ${
+              isNekoVisible ? "fill-[#A47764]" : "fill-white"
+            }`}
+            onClick={toggleNeko}
+          />
+        </span>
       </p>
     </section>
   );
