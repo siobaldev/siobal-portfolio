5581c6793a657f515fa76e5f9a3d8631c70e294c
br0wnrecluse <sblmnrd@gmail.com>
2025-08-28 10:12:11 +0800

    fix: replace div into main for accessibility

--- app/page.jsx
+++ app/page.jsx
@@ -6,15 +6,15 @@ import Contact from "@/components/contact/page";
 
 export default function HomePage() {
   return (
-    <div className="container relative lg:gap-y-20 flex flex-col lg:flex-row min-h-screen max-w-screen-[1200px] max-[400px]:px-6 gap-y-12 md:gap-y-0 sm:px-20 px-10 min-w-[320px] text-white/87">
+    <main className="container relative lg:gap-y-20 flex flex-col lg:flex-row min-h-screen max-w-screen-[1200px] max-[400px]:px-6 gap-y-12 md:gap-y-0 sm:px-20 px-10 min-w-[320px] text-white/87">
       <div className="grain -z-20 fixed flex items-center justify-center opacity-[0.02] inset-0 bg-no-repeat bg-cover" />
       <Header />
-      <main className="lg:w-1/2 lg:space-y-36 space-y-20 md:space-y-28 mb-[114px] md:py-20 lg:py-32">
+      <div className="lg:w-1/2 lg:space-y-36 space-y-20 md:space-y-28 mb-[114px] md:py-20 lg:py-32">
         <About />
         <Projects />
         <Technologies />
         <Contact />
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
